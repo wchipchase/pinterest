c234ca0ca32d44715e7f21d556022cccd9334f61
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-05-14 19:24:44 -0600

    boards are printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,9 +1,26 @@
 import boardData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+let boardsArray = [];
+
+const domStringBuilder = () => {
+  let domString = '';
+  boardsArray.forEach((board) => {
+    domString += '<div class="card col-3">';
+    domString += `<div class="card-header">${board.id}</div>`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">${board.name}</h5>`;
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+};
 
 const initBoards = () => {
   boardData.loadBoards()
     .then((resp) => {
-      console.error('resp', resp.data.boards);
+      boardsArray = resp.data.boards;
+      domStringBuilder();
     })
     .catch(err => console.error(err));
 };
